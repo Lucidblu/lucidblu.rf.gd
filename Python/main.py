4bc249c175228ad059697e5a1a519c524cecd2b0
Lucidblu <128106862+Lucidblu@users.noreply.github.com>
2023-05-25 23:01:48 -0400

    Add files via upload

---
+++ Python/main.py
@@ -0,0 +1,5 @@
+userInput = input("Type the number associated to the script you want to use \n 1. Password Generator \n 2. List all saved passwords \n")
+if userInput == "1":
+    import passgen
+if userInput == "2":
+    import passlist
---
+++ Python/passlist.py
@@ -0,0 +1,3 @@
+passList = open("Password.txt", "r")
+
+print(passList.read())
